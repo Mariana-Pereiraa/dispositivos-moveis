a8d07d93b167aa915e2cf91f087e398ab0bbf202
Mariana-Pereiraa <maris22003@gmail.com>
2025-02-05 15:47:44 -0300

    iniciando AuthApp

---
+++ AuthApp/app/src/main/java/com/example/authapp/viewmodel/AuthViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.authapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.authapp.data.AuthRepository
+import kotlinx.coroutines.launch
+
+class AuthViewModel(
+    private val repository: AuthRepository
+) : ViewModel(){
+    var loginResult : ((Boolean) -> Unit) ?= null
+    var registerResult: ((Boolean) -> Unit) ?= null
+
+    fun login(
+        email: String,
+        password: String,
+        onResult: (Boolean) -> Unit
+    ){
+        viewModelScope.launch {
+            val success = repository.loginUser(email, password)
+            onResult(success)
+        }
+    }
+
+    fun resetPassword(
+        email: String,
+        onResult: (Boolean) -> Unit
+    ){
+        viewModelScope.launch {
+            val success = repository.resetPassword(email)
+            onResult(success)
+        }
+    }
+}
